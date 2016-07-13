265e35c61c300b2acac142f6165becaae2557c37
Ajay krishnan <ajaykrishnan@qburst.com>
2016-07-13 17:48:55 +0530

    employee

--- employee.js
+++ employee.js
@@ -1,63 +1,80 @@
-var employees = [
-	{
-	    "name": "ajay",
-	    "salary": "12000"
-	}, 
-	{
-	    "name": "seena",
-	    "salary": "20000"
-	}, 
-	{
-	    "name": "ann",
-	    "salary": "25000"
-	}, 
-	{
-	    "name": "ravi",
-	    "salary": "45625"
-	}, 
-	{
-	    "name": "sooraj",
-	    "salary": "12456"
-	}, 
-	{
-	    "name": "hemand",
-	    "salary": "88888"
-	}, 
-	{
-	    "name": "sasi",
-	    "salary": "12323"
-	}, 
-	{
-	    "name": "thomas",
-	    "salary": "55648"
-	}, 
-	{
-	    "name": "alva",
-	    "salary": "20032"
-	}, 
-	{
-	    "name": "edison",
-	    "salary": "4526"
-	}, 
-	{
-	    "name": "anand",
-	    "salary": "78758"
-	}, 
-	{
-	    "name": "beena",
-	    "salary": "79474"
-	}, 
-	{
-	    "name": "xavier",
-	    "salary": "45456"
-	}, ],
-	tablearr;
-for(var i = 0, i<employees.length; i++) {
-    var temp = '<tr><td>' + data[i].id + '</td>';
-    temp+= '<td>' + data[i].name+ '</td>';
-    temp+= '<td>' + data[i].category + '</td>';
-    temp+= '<td>' + data[i].color + '</td></tr>';
-    tablearr.push(temp);
-}
+var employees = 
+[
+    {
+        "Name": "Ajay",
+        "Salary": 4125
+    }, 
+    {
 
-$('table tbody').append(tablearr.join(''));
+        "Name": "Seena",
+        "Salary": 1356
+    },
+     {
+
+        "Name": "Meena",
+        "Salary": 19056
+    },
+     {
+
+        "Name": "Robert",
+        "Salary": 12356
+    }, 
+    {
+
+        "Name": "Wade",
+        "Salary": 22356
+    }, 
+    {
+
+        "Name": "Messi",
+        "Salary": 5456
+    },
+     {
+
+        "Name": "Ann",
+        "Salary": 44210
+    }
+];
+function populateTable(filter) {
+    var name = document.getElementById('ename').value,
+        name = name.toLowerCase();
+
+    var col = [];
+    for (var i = 0; i < employees.length; i++) {
+        for (var key in employees[i]) {
+            if (col.indexOf(key) == -1) {
+                col.push(key);
+            }
+        }
+    }
+    var table = document.createElement("table");
+    table.setAttribute('id', 'demo')
+    var tr = table.insertRow(-1);
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+    var sum = 0;
+    for (var i = 0; i < employees.length; i++) {
+        var empName = employees[i].Name,
+            empName = empName.toLowerCase();
+
+        if ((filter && empName.search(name) > -1) || !filter) {
+            tr = table.insertRow(-1);
+            sum = sum + employees[i].Salary;
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = employees[i][col[j]];
+            }
+        }
+    }
+    var buildTable = document.getElementById("showData");
+    buildTable.innerHTML = "";
+    buildTable.appendChild(table);
+    var tr = table.insertRow(-1);
+    var tablCell = tr.insertCell(-1);
+    tablCell.innerHTML = "TOTAL";
+    var cellNext = tr.insertCell(-1);
+    cellNext.innerHTML = "$" + sum;
+}
